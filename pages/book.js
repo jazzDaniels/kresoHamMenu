8aa073ae4847342b1d98b986f2ea0d3480c62832
jazzDaniels <77404355+jazzDaniels@users.noreply.github.com>
2022-09-02 09:25:22 +0200

    ispravak

--- pages/book.js
+++ pages/book.js
@@ -6,11 +6,13 @@ export default function CalendarGfg() {
   const [value, onChange] = useState(new Date());
 
   return (
-    <div>
-      <h1 className="text-center text-2xl uppercase text-center antialiased font-bold text-pink-900 p-5">
-        Ugovori date sa Krešimirom - for free!
-      </h1>
-      <div className="ml-5 mb-20">
+    <div className="bg-hero h-screen w-screen bg-no-repeat bg-center bg-scroll bg-auto -mb-12">
+      <div className="mt-10">
+        <h1 className="text-center text-1xl uppercase text-center antialiased font-bold text-pink-900 mt-0">
+          Ugovori date sa Krešimirom - for free!
+        </h1>
+      </div>
+      <div className="flex flex-row justify-center mt-20">
         <Calendar onChange={onChange} value={value} />
       </div>
     </div>
